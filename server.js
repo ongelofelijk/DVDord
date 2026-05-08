e5e4753cf504d9f7e53fc56555b26a73df87a66b
ongelofelijk <a.juliaans@ll.lorentzlyceum.nl>
2026-05-08 11:26:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 let http = require('http');
-
+const express = require('express');
+const app = express();
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World!');
